4ab93dbe87c2529206718d378a6e4da0f3b223cd
Code4Daze <jevon.fillette@gmail.com>
2022-05-29 20:21:11 -0400

    Add functions for some buttons
    Created function for clear, delete, change sign, percentage

--- script.js
+++ script.js
@@ -19,3 +19,51 @@ let sixBtn = document.querySelector('#six');
 let sevenBtn = document.querySelector('#seven');
 let eightBtn = document.querySelector('#eight');
 let nineBtn = document.querySelector('#nine');
+
+clearBtn.addEventListener('click', () => output.textContent = "0");
+deleteBtn.addEventListener('click', removeLast);
+signChangeBtn.addEventListener('click', changeSign);
+percentageBtn.addEventListener('click', toPercent);
+decimalPointBtn.addEventListener('click', addDecimal);
+
+function addDecimal() {
+  if (output.textContent.includes(".")) {
+    output.textContent = output.textContent;
+  } else {
+    output.textContent += ".";
+  }
+}
+
+function toPercent() {
+  let display = +output.textContent;
+  let newDisplay = display / 100;
+  let str = newDisplay.toString();
+  if (str.includes("e")) {
+    let newStr = str.split("e");
+    let firstPart = newStr[0];
+    let secondPart = newStr[1];
+    let fullSecondPart = `e${secondPart}`;
+    let fullFirstPart = firstPart.substring(0, 7);
+    output.textContent = `${fullFirstPart}${fullSecondPart}`;
+  } else if (str === "NaN") {
+    output.textContent = "Too Small";
+  } else {
+    output.textContent = str.substring(0, 11);
+  }
+}
+
+function changeSign() {
+  let display = +output.textContent;
+  display *= -1;
+  output.textContent = display;
+}
+
+function removeLast() {
+  let display = output.textContent;
+  let str = display.slice(0, -1);
+  if (str === "") {
+    output.textContent = "0";
+  } else {
+    output.textContent = str;
+  }
+}
